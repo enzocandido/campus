1ffe699ac3f5c713524479faa0c292d3532697c5
Enzo Candido <ennzzc@gmail.com>
2024-10-08 00:16:55 -0300

    revert initial profile

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,39 +1,32 @@
 import { currentUser, auth } from "@clerk/nextjs/server";
+
 import { db } from "@/lib/db";
 
 export const initialProfile = async () => {
-  try {
-    const user = await currentUser();
+  const user = await currentUser();
 
-    if (!user) {
-      return auth().redirectToSignIn();
-    }
-
-    const profile = await db.user.findUnique({
-      where: {
-        id: user.id,
-      },
-    });
-
-    if (profile) {
-      return profile;
-    }
-
-    const fullName =
-      (user.firstName || "") + (user.lastName ? ` ${user.lastName}` : "");
-
-    const newProfile = await db.user.create({
-      data: {
-        id: user.id,
-        name: fullName || "Unknown",
-        imageUrl: user.imageUrl || "",
-        email: user.emailAddresses[0].emailAddress,
-      },
-    });
-
-    return newProfile;
-  } catch (error) {
-    console.error("Error creating or retrieving the user profile:", error);
-    throw new Error("Failed to create or retrieve user profile");
+  if (!user) {
+    return auth().redirectToSignIn();
   }
+
+  const profile = await db.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await db.user.create({
+    data: {
+      id: user.id,
+      name: (user.lastName ? `${user.firstName} ${user.lastName}` : `${user.firstName}`),
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
 };
