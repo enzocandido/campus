3cac3dc91ba1d09b4f68f6d82a16d64d766f04ce
Enzo Candido <ennzzc@gmail.com>
2024-10-09 23:11:03 -0300

    about fix

--- app/(about)/(routes)/about/page.tsx
+++ app/(about)/(routes)/about/page.tsx
@@ -1,49 +1,32 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-
-const HowToUse = () => {
-  const faqs = [
+const About = () => {
+  const sections = [
     {
-      question: "Como posso criar minha conta?",
-      answer:
-        "Para criar uma conta, selecione 'Registre-se' na página inicial, insira seu e-mail, crie uma senha segura e complete os dados necessários.",
+      title: "Sobre nós",
+      content:
+        "Campus é um recurso pedagógico desenvolvido para simplificar a interação e cooperação entre alunos e docentes, fomentando um ambiente de ensino mais interativo e acessível. Primeiramente disponibilizada como um site, ela será posteriormente ampliada para um aplicativo para dispositivos móveis, garantindo acesso fácil em qualquer lugar. Dentre suas funcionalidades primordiais estão um sistema de mensagens para comunicação ágil, um instrumento de administração de tarefas para estruturar as tarefas escolares e acompanhar prazos, áreas de colaboração para projetos coletivos e ferramentas de acessibilidade. Além disso, a plataforma prioriza a segurança dos dados dos usuários, adotando medidas rigorosas de proteção e oferecendo controle total sobre as informações pessoais."
     },
     {
-      question: "Como eu envio uma tarefa?",
-      answer:
-        "Entre na seção 'Tarefas', escolha 'Enviar Tarefa', escolha o arquivo ou preencha as informações exigidas e pressione 'Enviar'.",
+      title: "Nossa equipe",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus nec tellus eu tempor. Curabitur id leo ut mauris tincidunt euismod. Sed id leo ut libero egestas pulvinar nec sed urna. Quisque nec sem ac lorem pellentesque dignissim a ut neque. Morbi ullamcorper mi quis efficitur ultricies. Curabitur lobortis fringilla orci, sit amet vulputate turpis venenatis vel. Maecenas ut lorem ac eros consectetur laoreet sed et velit. Quisque venenatis, turpis tempor dapibus luctus, risus diam commodo nisl, non malesuada neque lorem ac velit. Integer vulputate risus hendrerit justo efficitur vehicula. Cras vel ligula sed nulla aliquet tincidunt non at lorem. Duis sit amet libero venenatis ipsum dictum accumsan a dignissim ante. Phasellus commodo, orci vel feugiat aliquet, eros neque tempor mi, eget tristique ante ligula vel eros. Cras eu congue nisi."
     },
     {
-      question: "Como posso me comunicar com meus professores e colegas?",
-      answer:
-        "Primeiramente, faça login no servidor da sua classe ou grupo. Depois, acesse 'Mensagens', selecione o membro com quem deseja interagir e envie sua mensagem diretamente.",
-    },
-    {
-      question: "Como redefino minha senha?",
-      answer:
-        "Se você esqueceu sua senha, selecione 'Esqueci a senha' na página de login. Um e-mail será enviado a você com instruções para definir uma nova senha.",
-    },
+      title: "O projeto",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus nec tellus eu tempor. Curabitur id leo ut mauris tincidunt euismod. Sed id leo ut libero egestas pulvinar nec sed urna. Quisque nec sem ac lorem pellentesque dignissim a ut neque. Morbi ullamcorper mi quis efficitur ultricies. Curabitur lobortis fringilla orci, sit amet vulputate turpis venenatis vel. Maecenas ut lorem ac eros consectetur laoreet sed et velit. Quisque venenatis, turpis tempor dapibus luctus, risus diam commodo nisl, non malesuada neque lorem ac velit. Integer vulputate risus hendrerit justo efficitur vehicula. Cras vel ligula sed nulla aliquet tincidunt non at lorem. Duis sit amet libero venenatis ipsum dictum accumsan a dignissim ante. Phasellus commodo, orci vel feugiat aliquet, eros neque tempor mi, eget tristique ante ligula vel eros. Cras eu congue nisi."
+    }
   ];
 
   return (
-    <div>
-      <h1 className="text-2xl mb-10 text-center">
-        Como Usar a Plataforma - Perguntas Frequentes
-      </h1>
-      <Accordion type="single" collapsible className="w-full">
-        {faqs.map((faq, index) => (
-          <AccordionItem key={index} value={`item-${index + 1}`}>
-            <AccordionTrigger>{faq.question}</AccordionTrigger>
-            <AccordionContent>{faq.answer}</AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+    <div className="md:flex justify-evenly">
+      {sections.map((section, index) => (
+        <div key={index} className="m-4">
+          <h1 className="text-2xl">{section.title}</h1>
+          <p className="mt-2 font-light">{section.content}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default HowToUse;
+export default About;
