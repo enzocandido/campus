53812df21a1104b30dec2cee21d68c58f530e664
Gabriel Ricardo <gabrielpelossi@yahoo.com.br>
2024-10-24 22:15:44 -0300

    home page adjustment

--- components/home/home-profile-card.tsx
+++ components/home/home-profile-card.tsx
@@ -31,32 +31,53 @@ export const HomeProfileCard = ({
   course,
 }: HomeProfileCardProps) => {
   return (
-    <div className="w-full h-full">
-      <Card className="text-center md:flex items-center justify-around p-4">
+    <div className="w-full h-screen flex justify-center items-start mt-0 px-2">
+      <Card className="w-full max-w-full mx-auto shadow-lg rounded-lg text-center p-6 mt-0">
         <CardHeader>
-          <CardTitle>{name}</CardTitle>
+          <CardTitle>Bem-vindo!</CardTitle>
+          <CardDescription>{name}</CardDescription>
           <CardDescription>{email}</CardDescription>
-          <Avatar className="flex justify-center">
+          <Avatar className="mx-auto mt-4">
             <AvatarImage
-              className="h-64 w-64 rounded-md"
+              className="h-32 w-32 rounded-full"
               src={imageUrl}
               alt={`Avatar de ${name}`}
             />
           </Avatar>
         </CardHeader>
-        <CardContent className="items-center">
-          <div className="mt-4 space-y-2">
-            {ra && <p className="text-sm">RA: {ra}</p>}
-            <p className="text-sm">Curso: {course}</p>
-            <p className="text-sm">Universidade: {university}</p>
+        <CardContent className="flex flex-col items-center">
+          <div className="mt-4 space-y-2 text-center">
+            {ra && <p className="text-sm font-semibold">RA: {ra}</p>}
+            <p className="text-sm font-semibold">Curso: {course}</p>
+            <p className="text-sm font-semibold">Universidade: {university}</p>
           </div>
         </CardContent>
-        <CardFooter className="justify-center">
-          <Button disabled className="flex items-center gap-2">
-            <Edit2 className="w-4 h-4" />
-            Editar
-          </Button>
-        </CardFooter>
+
+        <h2 className="mt-6 text-lg font-semibold">Aqui estão algumas dicas para começar</h2>
+
+        <div className="grid grid-cols-1 gap-4 mt-4 md:grid-cols-2">
+          <Card className="w-full max-w-full mx-auto shadow-lg rounded-lg text-center p-6 mt-0">
+            <CardTitle className="text-xl font-semibold">Criação de salas</CardTitle>
+            <CardContent>
+              <p className="text-gray-400 text-justify mt-2">
+              Você pode criar salas para promover uma interação mais dinâmica e colaborativa entre os alunos. Essas salas oferecem diversas funcionalidades, incluindo chat de texto para discussões em tempo real, chamadas de vídeo para uma comunicação mais próxima, ligações de áudio para conveniência e compartilhamento de arquivos para facilitar a troca de documentos e materiais de estudo.
+              </p>
+            </CardContent>
+          </Card>
+
+          <Card className="w-full max-w-full mx-auto shadow-lg rounded-lg text-center p-6 mt-0">
+            <CardTitle className="text-xl font-semibold">Como ver minhas tarefas?</CardTitle>
+            <CardContent>
+              <p className="text-gray-400 text-justify mt-2">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s com a liberação de folhas Letraset contendo passagens de Lorem Ipsum e mais recentemente com software de editoração eletrônica como Aldus PageMaker, incluindo versões de Lorem Ipsum.
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+
+        <div className="mt-4">
+          <a href="/howtouse" className="text-blue-500 hover:underline">Como usar a plataforma?</a>
+        </div>
       </Card>
     </div>
   );
